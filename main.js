52b56e95b1245ad2e53585913e68465f4e231554
Phillip Lewis <phillip.lewisjr@gmail.com>
2016-12-11 08:52:00 -0600

    completed exercise

--- main.js
+++ main.js
@@ -1,25 +1,78 @@
-var inputNum = document.getElementById("treeHeight");
-var inputNum = document.getElementById("treeElement");
 
 
 
-var tree = function(treeBuilder){
+    
+    var gatherInputAndBuildTree = function(){
+          var treeBuilder = gatherInput();
+          drawTree(treeBuilder);
+    } 
 
-  for (var i = 1; i <= treeBuilder.height.value; i++) {
-    var lineToWrite = '';
-    var numberOfSpacesToDraw = mytreeBuilder.height.value - i;
+    function keypressEvent(eventArgs) {
+      if (eventArgs.keyCode === 13) {
+        var inputErrorMessage = validateInput();
+          if(inputErrorMessage === ''){
+            gatherInputAndBuildTree();
+          }else{
+            alert(inputErrorMessage);
+          }
+      }
+    }
+//field validation function
+var validateInput = function(){
+  var heightFieldValue = document.getElementById("treeHeight").value; 
+  var characterFieldValue = document.getElementById("treeElement").value;
+  if (heightFieldValue === "") {
+    return "You must enter a numerical value for height";
+  }else if (characterFieldValue === "") {
+    return "You must enter a character value";
+  }else if(isNaN(heightFieldValue)){
+return "height must be a number";
+  } else if (characterFieldValue.length !== 1) {
+    return "Only one character is allowed.";
+  }
+  return "";
+};
+
+//input object data
+    function gatherInput () {
+    var mytreeBuilder = {};
+    var inputNum = document.getElementById("treeHeight");
+    var inputChar = document.getElementById("treeElement");
+    mytreeBuilder.height = inputNum.value;
+    mytreeBuilder.characters = inputChar.value;
+    return mytreeBuilder;
+    //tree(mytreeBuilder);
+    }
+
+
+    // tree building function
+   var drawTree = function(treeBuilder) {
+
+   for (var i = 1; i <= treeBuilder.height; i++) {
+     var lineToWrite = '';
+
+    var numberOfSpacesToDraw = treeBuilder.height - i;
     for (var j = 1; j <= numberOfSpacesToDraw; j++) {
       lineToWrite += ' ';
     }
-    var numberOfCharactersToDraw = (i -1) + i;
+    var numberOfCharactersToDraw = (i - 1) + i;
     for (var k = 1; k <= numberOfCharactersToDraw; k++) {
-      lineToWrite += treeBuilder.characters.value;
+      lineToWrite += treeBuilder.characters;
     }
     console.log(lineToWrite);
   }
 }
-  
-  var mytreeBuilder = {};
-  mytreeBuilder.height = {value: 7};
-  mytreeBuilder.characters = {value: 'a'}
-  tree(mytreeBuilder);
+
+
+
+
+  var b1 = document.getElementById("goGreen");
+  b1.addEventListener("click", function(){
+    var inputErrorMessage = validateInput();
+          if(inputErrorMessage === ''){
+            gatherInputAndBuildTree();
+          }else{
+            alert(inputErrorMessage);
+          }
+
+  });
